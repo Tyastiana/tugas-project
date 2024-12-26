d22bf89b9ec4d7b78c7bb272cebb577d23fd4664
Tyastiana <putrytiana125@gmail.com>
2024-12-26 20:32:43 +0700

    add dashboard admin

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -6,7 +6,9 @@ use Illuminate\Foundation\Configuration\Middleware;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
-        web: __DIR__.'/../routes/web.php',
+        web: [__DIR__.'/../routes/web.php',
+            __DIR__.'/../routes/admin.php',
+    ],
         commands: __DIR__.'/../routes/console.php',
         health: '/up',
     )
@@ -16,7 +18,11 @@ return Application::configure(basePath: dirname(__DIR__))
             \Illuminate\Http\Middleware\AddLinkHeadersForPreloadedAssets::class,
         ]);
 
-        //
+        $middleware->alias([
+            'role' => \Spatie\Permission\Middleware\RoleMiddleware::class,
+            'permission' => \Spatie\Permission\Middleware\PermissionMiddleware::class,
+            'role_or_permission' => \Spatie\Permission\Middleware\RoleOrPermissionMiddleware::class,
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
